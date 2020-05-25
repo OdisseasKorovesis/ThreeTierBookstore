7d03813c0e5786e963135aaf238f39a811c5628b
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-25 15:28:35 +0300

    Implement search logic

--- src/main/java/com/project/bookstore/service/SearchServiceImpl.java
+++ src/main/java/com/project/bookstore/service/SearchServiceImpl.java
@@ -1,7 +1,10 @@
 package com.project.bookstore.service;
 
+import com.project.bookstore.models.Author;
 import com.project.bookstore.models.Book;
+import com.project.bookstore.repository.RepositoryAuthors;
 import com.project.bookstore.repository.RepositoryBooks;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,9 +13,25 @@ public class SearchServiceImpl implements ISearchService {
     @Autowired
     private RepositoryBooks repositoryBooks;
 
+    @Autowired
+    private RepositoryAuthors repositoryAuthors;
+
     @Override
     public List<Book> searchBooks(String keyphrase) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Book> resultBooks = new ArrayList();
+        resultBooks.addAll(repositoryBooks.searchByTitle(keyphrase));
+        resultBooks.addAll(searchBooksByAuthorName(keyphrase));
+        return resultBooks;
+    }
+
+    private List<Book> searchBooksByAuthorName(String keyphrase) {
+        List<Book> resultBooks = new ArrayList();
+        List<Author> resultAuthors = repositoryAuthors.searchByFirstName(keyphrase);
+        resultAuthors.add(repositoryAuthors.searchByLastName(keyphrase));
+        for (Author author : resultAuthors) {
+            resultBooks.addAll(repositoryBooks.searchByAuthor(author));
+        }
+        return resultBooks;
     }
 
 }
