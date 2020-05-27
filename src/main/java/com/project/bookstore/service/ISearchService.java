7ef0cb01c611263503d0b9c80e180f564166cebb
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-27 22:42:12 +0300

    Improve search

--- src/main/java/com/project/bookstore/service/ISearchService.java
+++ src/main/java/com/project/bookstore/service/ISearchService.java
@@ -1,10 +1,10 @@
 package com.project.bookstore.service;
 
 import com.project.bookstore.models.Book;
-import java.util.List;
+import java.util.Set;
 
 public interface ISearchService {
 
-    List<Book> searchBooks(String keyphrase);
+    Set<Book> searchBooks(String keyphrase);
 
 }
