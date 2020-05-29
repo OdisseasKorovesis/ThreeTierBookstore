4cf4894f2b4936e029f9f9c9773c84d97b50776d
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-29 17:59:32 +0300

    Fix bug in genre repository where query was returning wrong type

--- src/main/java/com/project/bookstore/repository/RepositoryGenre.java
+++ src/main/java/com/project/bookstore/repository/RepositoryGenre.java
@@ -1,13 +1,12 @@
 package com.project.bookstore.repository;
 
 import com.project.bookstore.models.Genre;
-import com.project.bookstore.models.Language;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RepositoryGenre extends CrudRepository<Genre, Integer> {
 
     @Query(value = "SELECT * FROM genre WHERE name= ?1", nativeQuery = true)
-    Language findByName(String name);
+    Genre findByName(String name);
 
 }
