ab993d3b1a85b8bd5cfbf309bb23e77fc8208f19
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-30 14:50:15 +0300

    Add role and user controller

--- src/main/java/com/project/bookstore/repository/RepositoryUsers.java
+++ src/main/java/com/project/bookstore/repository/RepositoryUsers.java
@@ -1,24 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.project.bookstore.repository;
 
 import com.project.bookstore.models.User;
 import org.springframework.data.repository.CrudRepository;
-/**
- *
- * @author chatz
- */
-public interface RepositoryUsers extends CrudRepository<User, Integer>  {
-    
-   User findUserById(Integer id);
-    
+
+public interface RepositoryUsers extends CrudRepository<User, Integer> {
+
+    User findUserById(Integer id);
+
 }
-
-
-    
-   
-    
-
