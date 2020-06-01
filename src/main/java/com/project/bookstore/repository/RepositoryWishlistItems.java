49002375e4bfc1875e3c798f9c44273e46a248a5
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-06-01 16:13:27 +0300

    Fix wishlist workflow

--- src/main/java/com/project/bookstore/repository/RepositoryWishlistItems.java
+++ src/main/java/com/project/bookstore/repository/RepositoryWishlistItems.java
@@ -8,10 +8,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface RepositoryWishlistItems extends CrudRepository<WishlistItem, Integer> {
 
-    @Query(value = "SELECT * from wishlist_items \n"
-            + "JOIN wishlists ON wishlists.id=wishlist_items.wishlist_id\n"
-            + "WHERE user_id=?1", nativeQuery = true)
-    List<WishlistItem> findWishlistItemsWithUserId(int id);
+    @Query(value = "SELECT * from wishlist_items WHERE wishlist_id=?1", nativeQuery = true)
+    List<WishlistItem> findWishlistItemsWithWishlistId(int wishlistId);
 
     @Query(value = "SELECT * from wishlist_items WHERE book_id=?1", nativeQuery = true)
     Book findWishlistItemWithBookId(int id);
