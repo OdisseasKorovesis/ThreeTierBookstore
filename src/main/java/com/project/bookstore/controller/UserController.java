8aa411a1d944b4eddb96b4c5057a4320881fbbc7
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-06-02 15:13:50 +0300

    create userdetails endpoint

--- src/main/java/com/project/bookstore/controller/UserController.java
+++ src/main/java/com/project/bookstore/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,4 +67,10 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @GetMapping("/userdetails")
+    public ResponseEntity<Object> getUserCredentials() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return new ResponseEntity<>(principal, HttpStatus.OK);
+
+    }
 }
